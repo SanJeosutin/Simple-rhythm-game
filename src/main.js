c7f89de3342f853b09a5f37153b6d94b5c7b148e
Justin San <santosojustin@gmail.com>
2022-01-20 12:26:58 +1100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Phaser from "./lib/Phaser.js";
 import Game from "./scenes/Game.js";
 
 export default new Phaser.Game({
+    parent: "parent",
     type: Phaser.AUTO,
     width: 800,
     height: 600,
@@ -9,9 +10,6 @@ export default new Phaser.Game({
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { 
-                y: 200 
-            },
             debug: true
         }
     }
